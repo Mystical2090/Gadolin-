2d1be1fe3f6b2a2ce155715290091fdc5044d02d
Mystical433 <peterpanlee2015@gmail.com>
2025-08-13 23:22:30 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,15 @@
-#include "hacks.hpp"
+#include <Geode/Geode.hpp>
+#include "ui.hpp"
+#include <Geode/modify/CCKeyboardDispatcher.hpp>
+
+using namespace geode::prelude;
+
+class $modify(dickshit, cocos2d::CCKeyboardDispatcher) {
+    bool dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool isKeyDown, bool isKeyRepeat) {
+        if (key == cocos2d::enumKeyCodes::KEY_Tab && isKeyDown) {
+            geode::log::info("ui opened");
+            ui::create()->show();
+        }
+        return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat);
+    }
+};
